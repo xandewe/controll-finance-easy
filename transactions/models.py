58092a2bac6ad25a7ca8485107c9911852db3772
Desktop Alexandre <desktopalexandre@gmail.com>
2023-11-23 17:31:31 -0300

    feat: create model transaction

--- transactions/models.py
+++ transactions/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 
+class CategoryChoice(models.TextChoices):
+    INCOME = "income"
+    EXPENSE = "Expense"
+    UNDEFINED = "Undefined"
+
 class Transations(models.Model):
-    ...
+    name = models.CharField(max_length=120)
+    surname = models.CharField(max_length=100, ) # colocar como não obrigatório
+    value = models.DecimalField(max_digits=5, decimal_places=2)
+    received = models.BooleanField(default=True)
+    category = models.CharField(max_length=20, choices=CategoryChoice.choices, default=CategoryChoice.UNDEFINED)
+    created_at = models.DateTimeField(auto_now_add=True)
