6d051e6b9e221c678aea13714bf459996bcc2785
Desktop Alexandre <desktopalexandre@gmail.com>
2023-11-27 18:29:21 -0300

    initial test

--- transactions/tests.py
+++ transactions/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TransactionListCreateViewTest(APITestCase):
+    """
+    Classe desenvolvida para testar criação e listagem de transações
+    """
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.BASE_URL = "/transactions/"
+
+    def test_transaction_creation(self):
+        transaction_data = {
+            
+        }
+
+    def test_transaction_list(self):
+        ...
