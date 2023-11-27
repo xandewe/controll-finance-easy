fb1f43aaafaa9c1f19e1c6e65ce7f5006f053137
Desktop Alexandre <desktopalexandre@gmail.com>
2023-11-27 16:34:05 -0300

    feat: adjusment of the status and category field enum

--- transactions/models.py
+++ transactions/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 
 
-class CategoryChoice(models.TextChoices):
+class TransactionCategory(models.TextChoices):
     INCOME = "income"
     EXPENSE = "Expense"
-    UNDEFINED = "Undefined"
+
+
+class TransactionStatus(models.TextChoices):
+    PENDING = "Pending"
+    DONE = "Done"
 
 
 class Transaction(models.Model):
     name = models.CharField(max_length=120)
-    surname = models.CharField(
-        max_length=100,
-        null=True
-    )  # colocar como não obrigatório
+    surname = models.CharField(max_length=100, null=True)
     value = models.DecimalField(max_digits=5, decimal_places=2)
-    received = models.BooleanField(default=True)
+    status = models.CharField(
+        max_length=20,
+        choices=TransactionStatus.choices,
+    )
     credit_card = models.BooleanField(default=True)
     category = models.CharField(
-        max_length=20, choices=CategoryChoice.choices, default=CategoryChoice.UNDEFINED
+        max_length=20,
+        choices=TransactionCategory.choices,
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
