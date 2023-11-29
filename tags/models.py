b0d187400de00c9c56398928b940773669777383
Desktop Alexandre <desktopalexandre@gmail.com>
2023-11-29 19:39:15 -0300

    reafactor: add field sub_tag_name and choices in field tag_name

--- tags/models.py
+++ tags/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
 
+class TagName(models.TextChoices):
+    CASA = "Casa"
+    ALIMENTACAO = "Alimentacao"
+    SAUDE = "Saude"
+    ASSINATURAS = "Assinaturas"
+    TRANSPORTE = "Transporte"
+    EDUCACAO = "Educacao"
+    PET = "Pet"
+    LAZER = "Lazer"
+    OUTROS = "Outros"
+
+
 class Tag(models.Model):
-    tag_name = models.CharField(max_length=100, unique=True)
+    tag_name = models.CharField(
+        max_length=70,
+        choices=TagName.choices,
+    )
+    sub_tag_name = models.CharField(max_length=100, unique=True)
