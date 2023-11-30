fe657e86b316f5fe19818c375457d66b292bf2a0
Desktop Alexandre <desktopalexandre@gmail.com>
2023-11-30 17:50:19 -0300

    feat: upgraded filter in a new class

--- transactions/views.py
+++ transactions/views.py
@@ -4,8 +4,21 @@ from .models import Transaction
 from django_filters import rest_framework as filters
 
 
+class TransactionFilter(filters.FilterSet):
+    category = filters.CharFilter(field_name="category", lookup_expr="iexact")
+    tag = filters.CharFilter(field_name="tag__tag_name", lookup_expr="iexact")
+
+    class Meta:
+        model = Transaction
+        # fields = [
+        #     "category",
+        #     "tag__tag_name",
+        # ]
+        fields = ["category"]
+
+
 class TransactionView(generics.ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     filter_backends = (filters.DjangoFilterBackend,)
-    filterset_fields = ("category",)
+    filterset_class = TransactionFilter
