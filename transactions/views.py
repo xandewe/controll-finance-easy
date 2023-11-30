c0e0501abbc303c93417084045940e75cefd2f24
Desktop Alexandre <desktopalexandre@gmail.com>
2023-11-30 17:13:29 -0300

    feat: add new filter category

--- transactions/views.py
+++ transactions/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics
 from .serializers import TransactionSerializer
 from .models import Transaction
+from django_filters import rest_framework as filters
 
 
 class TransactionView(generics.ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ("category",)
