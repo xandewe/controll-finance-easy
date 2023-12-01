40bc6ff15d1a2a7206ad7db00d2baa92988b033b
Desktop Alexandre <desktopalexandre@gmail.com>
2023-12-01 20:16:53 -0300

    add new url for update, retrieve and destroy

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('transactions/', views.TransactionView.as_view()),
+    path("transactions/", views.TransactionView.as_view()),
+    path("transactions/<int:pk>/", views.TransactionDetailView.as_view()),
 ]
