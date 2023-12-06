3b80fd878fe025ce30041d181b1f615ea41d68cc
Desktop Alexandre <desktopalexandre@gmail.com>
2023-12-06 19:12:53 -0300

    feat: create custom command cli

---
+++ transactions/management/commands/populate.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand, CommandError
+from transactions.models import Transaction
+import csv
+import os
+from decimal import Decimal
+
+
+class Command(BaseCommand):
+    help = "read csv to populate the DB"
+
+    def add_arguments(self, parser):
+        parser.add_argument("file_csv")
+
+    def handle(self, *args, **options):
+        file_csv = os.path.join("./package_csv", options["file_csv"])
+
+        if os.path.exists(file_csv):
+            with open(file_csv, "r", encoding="utf-8") as file:
+                reader = csv.reader(file)
+                for line in reader:
+                    if reader.line_num > 1:
+                        fields = ["created_at", "value", "name"]
+                        line.pop(2)
+                        data = dict(zip(fields, line))
+                        data["value"] = Decimal(data["value"])
+                        data["created_at"] = "-".join(data["created_at"].split('/')[::-1])
+
+                        if data["value"] < 0:
+                            data.update({"category": "Expense"})
+                            data["value"] = -1 * data["value"]
+                        else:
+                            data.update({"category": "Income"})
+
+                        Transaction.objects.create(**data)
+
+                        self.stdout.write(
+                            self.style.SUCCESS(
+                                f"Dados processados para o DB com sucesso"
+                            )
+                        )
+
+        else:
+            self.stdout.write(
+                self.style.ERROR(f"Arquivo não encontrado no diretório | {file_csv}")
+            )
