37163fde4e8ec7cf6614e689f0d730c52d07f093
Desktop Alexandre <desktopalexandre@gmail.com>
2023-12-12 12:37:36 -0300

    feat: view for update and delete tag

--- tags/views.py
+++ tags/views.py
@@ -6,3 +6,8 @@ from .models import Tag
 class TagView(generics.ListAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+
+
+class TagDetailView(generics.UpdateAPIView, generics.DestroyAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
