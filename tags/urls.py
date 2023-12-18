6d3234d037c93621a4b2a9be776f73981e32ed93
Desktop Alexandre <desktopalexandre@gmail.com>
2023-12-18 10:31:14 -0300

    refactor: changed tag route name

--- tags/urls.py
+++ tags/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("transactions/tags/", views.TagView.as_view()),
-    path("transactions/tags/<int:pk>/", views.TagDetailView.as_view()),
+    path("tags/", views.TagView.as_view()),
+    path("tags/<int:pk>/", views.TagDetailView.as_view()),
 ]
