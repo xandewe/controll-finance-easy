0313d2a939766a9d5bbb4ac4083d6c5aebcceb54
alexandre <alexandrealvescs@gmail.com>
2024-01-05 10:13:15 -0300

    refactor: added name in path

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,7 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("transactions/", views.TransactionView.as_view()),
-    path("transactions/<int:pk>/", views.TransactionDetailView.as_view()),
-    path("transactions/<int:pk>/tag/", views.TransactionTagDeleteView.as_view()),
+    path(
+        "transactions/",
+        views.TransactionView.as_view(),
+        name="transaction-list-create",
+    ),
+    path(
+        "transactions/<int:pk>/",
+        views.TransactionDetailView.as_view(),
+        name="transaction-detail",
+    ),
+    path(
+        "transactions/<int:pk>/tag/",
+        views.TransactionTagDeleteView.as_view(),
+        name="transaction-delete-tag",
+    ),
 ]
