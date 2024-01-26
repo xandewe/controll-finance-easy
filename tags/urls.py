5673d9c121ba848a50bb8586b71ee865a4da9359
alexandre <alexandrealvescs@gmail.com>
2024-01-26 14:13:39 -0300

    feat: added name in url path

--- tags/urls.py
+++ tags/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("tags/", views.TagView.as_view()),
-    path("tags/<int:pk>/", views.TagDetailView.as_view()),
+    path(
+        "tags/",
+        views.TagView.as_view(),
+        name="tag-list",
+    ),
+    path(
+        "tags/<int:pk>/",
+        views.TagDetailView.as_view(),
+        name="tag-detail",
+    ),
 ]
