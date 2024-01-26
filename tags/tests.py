b4299e1a5965ba6f2c1415a720de6babe8786836
alexandre <alexandrealvescs@gmail.com>
2024-01-26 14:32:32 -0300

    feat: test added for tag list

--- tags/tests.py
+++ tags/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.views import status
+from django.urls import reverse
+from .models import Tag
 
-# Create your tests here.
+
+class TagListViewTest(APITestCase):
+    def setUp(self) -> None:
+        tag1 = Tag.objects.create(tag_name="Alimentacao", sub_tag_name="ifood")
+        tag2 = Tag.objects.create(tag_name="Saude", sub_tag_name="academia")
+
+    def test_tag_list(self):
+        URL = reverse("tag-list")
+
+        response = self.client.get(URL)
+
+        expected_status_code = status.HTTP_200_OK
+
+        msg = f"Verifique se o status code está conforme o solicitado"
+
+        self.assertEqual(expected_status_code, response.status_code, msg)
+
+        expected_pagination_keys = {"count", "next", "previous", "results"}
+        msg = f"Verifique se a paginação está sendo feita corretamente"
+
+        for expected_key in expected_pagination_keys:
+            self.assertIn(expected_key, response.json().keys(), msg)
+
+        results_len = response.json()["count"]
+        expected_len = 2
+
+        msg = "Verifique se a paginação está retornando apenas dois items de cada vez"
+
+        self.assertEqual(expected_len, results_len, msg)
+
+        expected_keys = {"id", "tag_name", "sub_tag_name"}
+
+        msg = f"Verifique se o serializer foi configurado para retornar os campos corretamente"
+
+        for key in expected_keys:
+            first_data = response.json()["results"][0].keys()
+
+            self.assertIn(key, first_data, msg)
+
+
+class TagDetailViewTest(APITestCase):
+    ...
