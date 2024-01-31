bf3b443338b72d814403975aac7dc7719f7df21e
alexandre <alexandrealvescs@gmail.com>
2024-01-31 17:59:34 -0300

    feat: creating user serializer

---
+++ users/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "password",
+        ]
+
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
