2f788c36437d3587cf909f15affe42bb4ec0dbb4
alexandre <alexandrealvescs@gmail.com>
2024-01-31 18:02:18 -0300

    feat: include users urls in application

--- _project/urls.py
+++ _project/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("transactions.urls")),
     path("api/", include("tags.urls")),
+    path("api/", include("users.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/schema/swagger/",
