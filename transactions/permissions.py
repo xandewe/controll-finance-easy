2f52131a28526f40df9e8151a816d045841cfe4b
alexandre <alexandrealvescs@gmail.com>
2024-02-14 09:27:13 -0300

    feat: custom permission for owner or superuser created

---
+++ transactions/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework import permissions
+from rest_framework.views import Request, View
+
+
+class IsOwnerOrSuperUser(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj or request.user.is_superuser
