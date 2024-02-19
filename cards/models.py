96ed4d4b9353c5f49968ab1424b1ea899166f9e3
alexandre <alexandrealvescs@gmail.com>
2024-02-19 10:21:00 -0300

    feat: model Card created

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TransactionType(models.TextChoices):
+    ACCOUNT = "Account"
+    CREDIT_CARD = "Credit Card"
+
+class Card(models.Model):
+    card_name = models.CharField(max_length=100)
+    category = models.CharField(
+        max_length=30,
+        choices = TransactionType.choices
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="card")
