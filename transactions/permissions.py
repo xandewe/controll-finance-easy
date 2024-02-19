d39429d93eb4f3bfc225349ca7a8d631d67132d3
alexandre <alexandrealvescs@gmail.com>
2024-02-19 09:32:44 -0300

    feat: added type hint

--- transactions/permissions.py
+++ transactions/permissions.py
@@ -3,5 +3,5 @@ from rest_framework.views import Request, View
 
 
 class IsOwnerOrSuperUser(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request: Request, view: View, obj):
         return request.user == obj.user or request.user.is_superuser
