f410169e2f3707b6676f5b61eee3ea775a39305a
alexandre <alexandrealvescs@gmail.com>
2024-02-19 10:37:49 -0300

    refactor: code formated

--- cards/models.py
+++ cards/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class TransactionType(models.TextChoices):
     ACCOUNT = "Account"
     CREDIT_CARD = "Credit Card"
 
+
 class Card(models.Model):
     card_name = models.CharField(max_length=100)
-    category = models.CharField(
-        max_length=30,
-        choices = TransactionType.choices
-    )
+    category = models.CharField(max_length=30, choices=TransactionType.choices)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="card")
+
 
 class CreditCardDetail(models.Model):
     due_date = models.IntegerField()
     closing_date = models.IntegerField()
     updated_at = models.DateTimeField(auto_now=True)
 
-    card = models.OneToOneField("cards.Card", on_delete=models.SET_NULL, related_name="card_detail")
+    card = models.OneToOneField(
+        "cards.Card", on_delete=models.SET_NULL, related_name="card_detail"
+    )
