6dbf460ee4554a60df98786adfe50c0606941926
alexandre <alexandrealvescs@gmail.com>
2024-02-20 10:01:48 -0300

    refactor: serializer CardSerializer position changed

--- cards/serializers.py
+++ cards/serializers.py
@@ -2,15 +2,15 @@ from rest_framework import serializers
 from .models import Card, CreditCardDetail
 
 
-class CardSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Card
-        fields = ["id", "card_name", "category", "card_detail"]
-
-
 class CreditCardDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = CreditCardDetail
         fields = ["id", "due_date", "closing_date", "updated_at"]
         read_only_fields = ["updated_at"]
+
+
+class CardSerializer(serializers.ModelSerializer):
+    card_detail = CreditCardDetailSerializer()
+    class Meta:
+        model = Card
+        fields = ["id", "card_name", "category", "card_detail"]
