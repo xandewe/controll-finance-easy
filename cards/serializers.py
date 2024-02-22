7d794ae4096a58ee92c2e74328904524a33497cf
alexandre <alexandrealvescs@gmail.com>
2024-02-22 10:55:50 -0300

    feat: added valid date check

--- cards/serializers.py
+++ cards/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Card, CreditCardDetail
+from .exceptions import InvalidDateException
 
 
 class CreditCardDetailSerializer(serializers.ModelSerializer):
@@ -21,6 +22,10 @@ class CardSerializer(serializers.ModelSerializer):
         card = Card.objects.create(**validated_data)
 
         if card_detail_data:
+            for _, value in card_detail_data.items():
+                if value < 0 or value > 31:
+                    raise InvalidDateException()
+                
             CreditCardDetail.objects.create(card=card, **card_detail_data)
 
         return card
