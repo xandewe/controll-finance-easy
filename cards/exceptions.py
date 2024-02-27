fb5a9fedde82dd45cdb1ad37ffd2fde61c89dd25
alexandre <alexandrealvescs@gmail.com>
2024-02-27 15:21:32 -0300

    feat: creation custom exception UserDoesNotContainsCardException

--- cards/exceptions.py
+++ cards/exceptions.py
@@ -3,3 +3,7 @@ from rest_framework.exceptions import APIException, status
 class InvalidDateException(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = 'Date invalid, enter a date valid between 1 a 31.'
+
+class UserDoesNotContainsCardException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'Card does not belong to the authenticated user.'
